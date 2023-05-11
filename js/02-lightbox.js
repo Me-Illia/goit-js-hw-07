8348f94dfdbee7cea0e3f8a33fc0a13a4ca18307
Me-Illia <sq999@ukr.net>
2023-05-11 12:55:31 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainerRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `        
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image"
+          src="${preview}"
+          alt="${description}" />
+        </a>
+        `
+    }).join('');
+};
+
+galleryContainerRef.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+  captionsData: 'alt',  
+  captionType: "attr",
+  captionDelay: 250
+});
+
+lightbox.on("show.simplelightbox");
